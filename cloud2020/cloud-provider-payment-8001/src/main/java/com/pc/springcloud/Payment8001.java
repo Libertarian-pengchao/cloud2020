94416ae1d92b97ce070759629a0cd3f7fb068cc5
pengchao <1518651315@qq.com>
2020-06-29 16:46:23 +0800

    8001改造为eureka客户端并注册进eureka服务端7001

--- cloud2020/cloud-provider-payment-8001/src/main/java/com/pc/springcloud/Payment8001.java
+++ cloud2020/cloud-provider-payment-8001/src/main/java/com/pc/springcloud/Payment8001.java
@@ -3,6 +3,7 @@ package com.pc.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ：pengchao
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.pc.springcloud.dao")
+@EnableEurekaClient
 public class Payment8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(Payment8001.class, args);
