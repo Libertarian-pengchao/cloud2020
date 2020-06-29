ae1371c2e5206130d87a343fb9a7fe2583cb0aff
pengchao <1518651315@qq.com>
2020-06-29 16:44:07 +0800

    将消费端改造为eureka客户端并注册到eureka服务端

--- cloud2020/cloud-consumer-order80/src/main/java/com/pc/springcloud/OrderMain80Application.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/pc/springcloud/OrderMain80Application.java
@@ -2,12 +2,14 @@ package com.pc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ：pengchao
  * @date ：2020-06-29
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80Application {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMain80Application.class, args);
